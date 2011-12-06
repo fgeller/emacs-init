0c6735e6956a03b14fa0277ad299097cfdf8c80e
Felix Geller <fgeller@gmail.com>
2011-12-06 13:13:26 +0100

    Solarized colors for vc-annotate.

---
+++ init-vc.el
@@ -0,0 +1,14 @@
+(setq vc-annotate-background "#fcf4dc")
+(setq vc-annotate-color-map
+      '((20 . "#a57705")
+        (40 . "#bd3612")
+        (50 . "#c60007")
+        (60 . "#c61b6e")
+        (80 . "#5859b7")
+        (100 ."#2075c7")
+        (120 ."#259185")
+        (140 ."#728a05")))
+
+(setq vc-annotate-very-old-color "#042028")
+
+(provide 'init-vc)
