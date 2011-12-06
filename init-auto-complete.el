cd7956c81cb5042099fe90b9fbe42091f1a268e6
Felix Geller <fgeller@gmail.com>
2011-12-06 11:12:28 +0100

    Don't modify completion-at-point for now.

--- init-auto-complete.el
+++ init-auto-complete.el
@@ -19,13 +19,13 @@
 ;;----------------------------------------------------------------------------
 ;; Use Emacs' built-in TAB completion hooks to trigger AC (Emacs >= 23.2)
 ;;----------------------------------------------------------------------------
-(setq tab-always-indent 'complete)  ;; use 'complete when auto-complete is disabled
-(add-to-list 'completion-styles 'initials t)
+;; (setq tab-always-indent 'complete)  ;; use 'complete when auto-complete is disabled
+;; (add-to-list 'completion-styles 'initials t)
 
 ;; hook AC into completion-at-point
-(defun set-auto-complete-as-completion-at-point-function ()
-  (setq completion-at-point-functions '(auto-complete)))
-(add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
+;; (defun set-auto-complete-as-completion-at-point-function ()
+;;   (setq completion-at-point-functions '(auto-complete)))
+;; (add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
 
 (set-default 'ac-sources
              '(;; ac-source-abbrev
