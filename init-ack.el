bea0ba435061b16dc9872f0e4f83d7a6252293ec
Felix Geller <fgeller@gmail.com>
2012-02-12 17:13:06 +0100

    Configure nschum's full-ack.

---
+++ init-ack.el
@@ -0,0 +1,9 @@
+(autoload 'ack-same "full-ack" nil t)
+(autoload 'ack "full-ack" nil t)
+(autoload 'ack-find-same-file "full-ack" nil t)
+(autoload 'ack-find-file "full-ack" nil t)
+
+(setq ack-arguments '("--ignore-dir=env"))
+
+
+(provide 'init-ack)
