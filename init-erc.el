569e920f11a36dfa603a5fcb8292dd7fcfa87d0d
Felix Geller <fgeller@gmail.com>
2012-02-12 19:23:57 +0100

    Open a new frame on being queried in ERC and add todochiku notification when receiving a PRIVMSG.
    Adding a hook to erc-server-PRIVMSG-functions doesn't work as expected
    at the moment. The notifications work fine, but the message is not
    propagated to the actual chat window :(

--- init-erc.el
+++ init-erc.el
@@ -1,4 +1,17 @@
 (setq erc-nick "felix^^")
 (setq erc-hide-list '("JOIN" "PART" "QUIT" "NICK"))
+(setq erc-auto-query 'frame)
+
+(defun fg/notify-privmsg (proc parsed)
+  (let ((nick (car (erc-parse-user (erc-response.sender parsed))))
+        (target (car (erc-response.command-args parsed)))
+        (msg (erc-response.contents parsed)))
+    (when (and (erc-current-nick-p target)
+               (not (erc-is-message-ctcp-and-not-action-p msg)))
+      (message "ERC message")))
+  nil)
+
+(add-hook 'erc-server-PRIVMSG-functions 'fg/notify-privmsg t)
+
 
 (provide 'init-erc)
