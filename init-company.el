933f8787d0b4d4e74677aee4575f877c91df7dbf
Felix Geller <fgeller@gmail.com>
2012-02-12 19:32:32 +0100

    Default config and mode-specific config for company-mode.
     - Empty backends by default and set them specifically for python- and
       emacs-lisp-mode
    
     - Shorten the delay before comany mode suggests completions.

--- init-company.el
+++ init-company.el
@@ -1,3 +1,23 @@
 (autoload 'company-mode "company" nil t)
 
+(setq company-idle-delay 0.1)
+(setq company-backends nil)
+
+(add-hook 'python-mode-hook (lambda ()
+                              (setq company-backends '((
+                                                       company-keywords
+                                                       company-etags
+                                                       company-files
+                                                       )))
+                              (company-mode 1)))
+
+(add-hook 'emacs-lisp-mode-hook (lambda ()
+                                  (setq company-backends '((
+                                                            company-elisp
+                                                            company-files
+                                                            )))
+                                  (company-mode 1)))
+
+
+
 (provide 'init-company)
