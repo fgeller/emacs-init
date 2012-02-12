31cd0f8ebe3d865175592b70d8b6a75c2670f8b4
Felix Geller <fgeller@gmail.com>
2012-02-12 17:19:22 +0100

    Exclude strings from spell-checking and don't spell-check Python file by default.

--- init-flyspell.el
+++ init-flyspell.el
@@ -1,13 +1,17 @@
 ;;----------------------------------------------------------------------------
 ;; Add spell-checking in comments for all programming language modes
 ;;----------------------------------------------------------------------------
+
+;; Manually exclude `font-lock-string-face' from spell-checking
+(setq flyspell-prog-text-faces
+      '(font-lock-comment-face font-lock-doc-face))
+
 (dolist (hook '(lisp-mode-hook
                 emacs-lisp-mode-hook
                 scheme-mode-hook
                 clojure-mode-hook
                 ruby-mode-hook
                 yaml-mode
-                python-mode-hook
                 shell-mode-hook
                 php-mode-hook
                 css-mode-hook
