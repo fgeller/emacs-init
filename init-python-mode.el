40013b0e9063b3a4772f3f72bbf06f49208fcbcf
Felix Geller <fgeller@gmail.com>
2012-02-13 23:46:53 +0100

    Revamped python initialization.
     - Use outline-minor-mode
    
     - Move whitespace settings into dir-locals.
    
     - Drop rope until it's fast.
    
     - Drop pdb configuration until we fix it.
    
     - Cleaner separation of configuration.

--- init-python-mode.el
+++ init-python-mode.el
@@ -1,32 +1,33 @@
-(require 'python)
+;;;;;;;;;;;;
+;; Pymacs ;;
+;;;;;;;;;;;;
+(require 'pymacs "~/.emacs.d/site-lisp/Pymacs/pymacs.el")
 
-(autoload 'doctest-mode "doctest-mode" "Python doctest editing mode." t)
+;;;;;;;;;;;;;;;;;
+;; python-mode ;;
+;;;;;;;;;;;;;;;;;
+(setq py-install-directory "~/.emacs.d/addons/python-mode")
+(setq pdb-path "/System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/pdb.py")
+(setq py-smart-indentation nil)
+(setq py-shell-name "ipython")
+(setq py-outline-minor-mode-p nil)
+(require 'python-mode)
 
-(setq auto-mode-alist
-      (append '(("\\.py$" . python-mode)
-                ("SConstruct$" . python-mode)
-                ("SConscript$" . python-mode))
-              auto-mode-alist))
+;;;;;;;;;;;;;;
+;; pylookup ;;
+;;;;;;;;;;;;;;
+(setq pylookup-dir "~/.emacs.d/addons/pylookup")
+(eval-when-compile (require 'pylookup))
+(setq pylookup-program (concat pylookup-dir "/pylookup.py"))
+(setq pylookup-db-file (concat pylookup-dir "/pylookup.db"))
+(setq pylookup-search-options '("--insensitive" "0" "--desc" "0"))
 
-(setq interpreter-mode-alist
-      (cons '("python" . python-mode) interpreter-mode-alist))
+(autoload 'pylookup-lookup "pylookup" t)
+(autoload 'pylookup-update "pylookup" t)
 
-(add-hook 'python-mode-hook
-          (lambda ()
-            (make-variable-buffer-local 'tab-width)
-            (make-variable-buffer-local 'indent-tabs-mode)
-            (make-variable-buffer-local 'whitespace-style)
-            (add-hook 'before-save-hook 'whitespace-cleanup nil 'local)
-            (imenu-add-menubar-index)
-            (hs-minor-mode t)
-            (setq
-             tab-width 2
-             python-indent 2
-             indent-tabs-mode t
-             python-guess-indent nil
-             whitespace-style '(face tabs spaces trailing lines space-before-tab::tab newline indentation::tab empty space-after-tab::tab space-mark tab-mark newline-mark)
-             )))
-
+;;;;;;;;;;;;;
+;; Flymake ;;
+;;;;;;;;;;;;;
 (defun flymake-python-init ()
   (let* ((temp-file (flymake-init-create-temp-buffer-copy
                      'flymake-create-temp-inplace))
@@ -38,93 +39,38 @@
 (add-to-list 'flymake-allowed-file-name-masks
              '("\\.py\\'" flymake-python-init))
 
+
+;; cf. http://stackoverflow.com/questions/4079648/combine-python-mode-with-org-mode-for-emacs/4093889#4093889
+
+(defun py-outline-level ()
+  (let (buffer-invisibility-spec)
+    (save-excursion
+      (skip-chars-forward "\t")
+      (current-column))))
+
+(defun fg/python-outline-hook ()
+  (setq outline-regexp "[ \t]*# \\|[ \t]+\\(class\\|def\\|if\\|elif\\|else\\|while\\|for\\|try\\|except\\|with\\) ")
+  (setq outline-level 'py-outline-level)
+  (outline-minor-mode t))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Buffer local variables and minor modes ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (add-hook 'python-mode-hook
-       (lambda () (unless (eq buffer-file-name nil) (flymake-mode 1))))
-
-;; Initialize Pymacs
-;; (autoload 'pymacs-apply "pymacs")
-;; (autoload 'pymacs-call "pymacs")
-;; (autoload 'pymacs-eval "pymacs" nil t)
-;; (autoload 'pymacs-exec "pymacs" nil t)
-;; (autoload 'pymacs-load "pymacs" nil t)
-;; Initialize Rope
-;; (pymacs-load "ropemacs" "rope-")
-;; (setq ropemacs-enable-autoimport t)
-;; (ac-ropemacs-initialize)
-;; http://gunnarwrobel.de/wiki/Python.html
-;; (setq pdb-path '/usr/lib/python2.7/pdb.py
-;;       gud-pdb-command-name 'pdb)
-;; http://lists.gnu.org/archive/html/help-gnu-emacs/2003-10/msg00577.html
-;; (defadvice pdb (before gud-query-cmdline activate)
-;;   "Provide a better default command line when called interactively."
-;;   (interactive
-;;    (list (gud-query-cmdline pdb-path
-;;              (file-name-nondirectory buffer-file-name)))))
-
-;; (add-hook 'python-mode-hook
-;;           (lambda ()
-;;          ;; (setq ac-sources (add-to-list 'ac-sources 'ac-source-ropemacs))))
-;;          (setq ac-sources (add-to-list 'ac-sources))))
-;; (add-hook 'python-mode-hook
-;;        (lambda () (unless (eq buffer-file-name nil) (flymake-mode 1))))
-;; http://code.google.com/p/autopair/
-;; (add-hook 'python-mode-hook
-;;        #'(lambda ()
-;;            (setq autopair-handle-action-fns
-;;                  (list #'autopair-default-handle-action
-;;                        #'autopair-python-triple-quote-action))))
-
-;; python
-;; (require 'python)
-;; Initialize Pymacs
-;; (autoload 'pymacs-apply "pymacs")
-;; (autoload 'pymacs-call "pymacs")
-;; (autoload 'pymacs-eval "pymacs" nil t)
-;; (autoload 'pymacs-exec "pymacs" nil t)
-;; (autoload 'pymacs-load "pymacs" nil t)
-;; Initialize Rope
-;; (pymacs-load "ropemacs" "rope-")
-;; (setq ropemacs-enable-autoimport t)
-;; (ac-ropemacs-initialize)
-;; http://gunnarwrobel.de/wiki/Python.html
-;; (setq pdb-path '/usr/lib/python2.7/pdb.py
-;;       gud-pdb-command-name 'pdb)
-;; http://lists.gnu.org/archive/html/help-gnu-emacs/2003-10/msg00577.html
-;; (defadvice pdb (before gud-query-cmdline activate)
-;;   "Provide a better default command line when called interactively."
-;;   (interactive
-;;    (list (gud-query-cmdline pdb-path
-;;				(file-name-nondirectory buffer-file-name)))))
-;; (defun flymake-pyflakes-init ()
-;;   (let* ((temp-file (flymake-init-create-temp-buffer-copy
-;;                   'flymake-create-temp-inplace))
-;;       (local-file (file-relative-name
-;;                    temp-file
-;;                    (file-name-directory buffer-file-name))))
-;;	(list "pycheckers"  (list local-file))))
-;; (add-to-list 'flymake-allowed-file-name-masks
-;;           '("\\.py\\'" flymake-pyflakes-init))
-;; Hooks
-;; (add-hook 'python-mode-hook
-;;        (lambda ()
-;;			(setq tab-width 4)
-;;			(setq indent-tabs-mode t)))
-;; (add-hook 'python-mode-hook
-;;           (lambda ()
-;;			;; (setq ac-sources (add-to-list 'ac-sources 'ac-source-ropemacs))))
-;;			(setq ac-sources (add-to-list 'ac-sources))))
-;; (add-hook 'python-mode-hook
-;;        (lambda () (unless (eq buffer-file-name nil) (flymake-mode 1))))
-;; http://code.google.com/p/autopair/
-;; (add-hook 'python-mode-hook
-;;        #'(lambda ()
-;;            (setq autopair-handle-action-fns
-;;					(list #'autopair-default-handle-action
-;;                        #'autopair-python-triple-quote-action))))
-;; (add-hook 'python-mode-hook
-;;           (lambda ()
-;;             ;; Scan the file for nested code blocks
-;;             (imenu-add-menubar-index)
-;;             (hs-minor-mode t)))
+          (lambda ()
+            (wrap-region-mode 1)
+            ;; pylookup
+            (make-local-variable browse-url-browser-function)
+            (setq browse-url-browser-function 'w3m)
+            ;; update ctags
+            (setq ctags-update-delay-seconds (* 5 60))
+            (ctags-update-minor-mode 1)
+            ;; flymake
+            (unless (eq buffer-file-name nil)
+              (flymake-mode 1))
+            ;; outline-minor-mode for folding
+            (fg/python-outline-hook)
+            ;; whitespace cleanup
+            (add-hook 'before-save-hook 'whitespace-cleanup nil 'local)))
 
 (provide 'init-python-mode)
