17b4632a69ced178cdd0893b3f0bac7b39a4139f
Felix Geller <fgeller@gmail.com>
2012-02-16 10:53:38 +0100

    Switching to fgallina's python.el

--- init-python-mode.el
+++ init-python-mode.el
@@ -6,12 +6,19 @@
 ;;;;;;;;;;;;;;;;;
 ;; python-mode ;;
 ;;;;;;;;;;;;;;;;;
-(setq py-install-directory "~/.emacs.d/addons/python-mode")
-(setq pdb-path "/System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/pdb.py")
-(setq py-smart-indentation nil)
-(setq py-shell-name "ipython")
-(setq py-outline-minor-mode-p nil)
-(require 'python-mode)
+(setq python-shell-interpreter "ipython"
+      python-shell-interpreter-args ""
+      python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+      python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+      python-shell-completion-setup-code
+      "from IPython.core.completerlib import module_completion"
+      python-shell-completion-module-string-code
+      "';'.join(module_completion('''%s'''))\n"
+      python-shell-completion-string-code
+      "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+(require 'python)
+
+
 
 ;;;;;;;;;;;;;;
 ;; pylookup ;;
