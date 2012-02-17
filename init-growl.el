c0f7177d36cad8f3d0c04d5aed6b4a5d33ee44cb
Felix Geller <fgeller@gmail.com>
2012-02-17 13:54:57 +0100

    Drop the default notification when compilation finishes and make mine sticky.

--- init-growl.el
+++ init-growl.el
@@ -1,11 +1,12 @@
 (require 'todochiku) ;; growl notifications when compilation finishes
 (setq todochiku-icons-directory (expand-file-name "~/.emacs.d/site-lisp/todochiku-icons"))
+(setq todochiku-compile-message nil)
 
 (add-hook 'compilation-finish-functions
           (lambda (buf msg)
             (todochiku-message "*compilation*"
                                msg
-                               (todochiku-icon 'emacs))))
-
+                               (todochiku-icon 'emacs)
+                               t)))
 
 (provide 'init-growl)
