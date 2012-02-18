2ebc3374f7e21d9da184359ea4f2d138ae12b622
Felix Geller <fgeller@gmail.com>
2012-02-18 15:29:23 +0100

    Let's default to ~/.emacs.d/info and that alone.

--- init-info.el
+++ init-info.el
@@ -1,7 +1,4 @@
 (add-to-list 'Info-directory-list "~/.emacs.d/info")
 
-(if *is-a-mac*
-    (add-to-list 'Info-directory-list
-                 "/Applications/Emacs.app/Contents/Resources/info"))
 
 (provide 'init-info)
