9324e6682adb79f9dfc72ad25aed2a9d1efb6252
Felix Geller <fgeller@gmail.com>
2012-02-24 12:21:03 +0100

    No default todochiku message upon a completed compilation. Factor out notification code into a separate function.

--- init-growl.el
+++ init-growl.el
@@ -2,12 +2,14 @@
 (setq todochiku-icons-directory (expand-file-name "~/.emacs.d/site-lisp/todochiku-icons"))
 (setq todochiku-compile-message nil)
 (setq todochiku-tooltip-too t)
+(setq compilation-finish-functions nil)
+
+(defun fg/notify-finished-compilation (buf msg)
+  (todochiku-message "*compilation*"
+                     msg
+                     (todochiku-icon 'emacs)))
 
 (add-hook 'compilation-finish-functions
-          (lambda (buf msg)
-            (todochiku-message "*compilation*"
-                               msg
-                               (todochiku-icon 'emacs)
-                               t)))
+          'fg/notify-finished-compilation)
 
 (provide 'init-growl)
