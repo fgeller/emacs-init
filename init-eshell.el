1d889254211ea0a65761f7d2d9058faae8a6e003
Felix Geller <fgeller@gmail.com>
2012-02-27 12:32:56 +0100

    Basic eshell configuration.

--- init-eshell.el
+++ init-eshell.el
@@ -1,2 +1,12 @@
+;; http://www.masteringemacs.org/articles/2010/12/13/complete-guide-mastering-eshell/
+(require 'eshell)
+(require 'em-term)
+(eval-after-load 'eshell
+  '(add-to-list 'eshell-visual-commands "htop"))
+
+(require 'em-smart)
+(setq eshell-where-to-jump 'begin)
+(setq eshell-review-quick-commands nil)
+(setq eshell-smart-space-goes-to-end t)
 
 (provide 'init-eshell)
