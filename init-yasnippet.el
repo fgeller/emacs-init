0444e3e7497fdbaf812bd0316d45cee699744f53
Felix Geller <fgeller@gmail.com>
2012-03-18 15:21:09 +0100

    Github's yasnippet config.

--- init-yasnippet.el
+++ init-yasnippet.el
@@ -1,6 +1,6 @@
 (require 'yasnippet)
-(yas/initialize)
-(yas/load-directory "~/.emacs.d/addons/yasnippet/snippets/text-mode")
+(yas/global-mode 1)
+(yas/load-directory "~/.emacs.d/addons/yasnippet/snippets")
 (setq yas/prompt-functions '(yas/dropdown-prompt))
 
 (provide 'init-yasnippet)
