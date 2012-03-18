f1b779c5825e422d85e8bbb6d6dc27ebfe987678
Felix Geller <fgeller@gmail.com>
2012-03-18 16:23:44 +0100

    Drop notmuch config.

--- init-email.el
+++ init-email.el
@@ -17,41 +17,4 @@
     (if (re-search-forward "mode=sign>" (point-at-eol) t)
         (newline))))
 
-;; notmuch
-(require 'notmuch)
-(require 'org-notmuch)
-(setq notmuch-fcc-dirs nil
-      notmuch-mua-user-agent-function 'notmuch-mua-user-agent-emacs
-      notmuch-saved-searches '(("Sent Mail" . "from:fgeller")
-                               ("inbox" . "tag:inbox")
-                               ("unread" . "tag:unread"))
-      notmuch-search-oldest-first nil
-      notmuch-show-logo nil
-      notmuch-crypto-process-mime t)
-
-(defun fg/decrypt-inlined-messages-in-buffer ()
-  (save-excursion
-    (let ((prefix "-----BEGIN PGP MESSAGE-----")
-          (suffix "-----END PGP MESSAGE-----")
-          start end)
-      (goto-char (point-max))
-      (while (re-search-backward suffix (point-min) t)
-        (setq end (point))
-        (re-search-backward prefix)
-        (setq start (point))
-        (epa-decrypt-region start end)))))
-(add-hook 'notmuch-show-hook 'fg/decrypt-inlined-messages-in-buffer)
-
-(define-key notmuch-search-mode-map "Q"
-  (lambda ()
-    (interactive)
-    (notmuch-search-operate-all "-unread -inbox")
-    (notmuch-search-quit)))
-
-;; bbdb
-;; (require 'bbdb)
-;; (bbdb-initialize 'message)
-;; (setq bbdb-complete-mail-allow-cycling t)
-;; (setq bbdb-completion-display-record nil)
-
 (provide 'init-email)
