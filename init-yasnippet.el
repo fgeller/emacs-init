c5b249295f5b0105f6a08e190bc7ef60b554afa9
Felix Geller <fgeller@gmail.com>
2012-03-20 17:25:17 +0100

    Changed snippet structure to new defaults.
     - Default snippets directory is now in ~/.emacs.d/snippets

--- init-yasnippet.el
+++ init-yasnippet.el
@@ -1,6 +1,5 @@
 (require 'yasnippet)
 (yas/global-mode 1)
-(yas/load-directory "~/.emacs.d/addons/yasnippet/snippets")
-(setq yas/prompt-functions '(yas/dropdown-prompt))
+
 
 (provide 'init-yasnippet)
