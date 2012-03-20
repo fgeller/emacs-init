40704b0670440132d93ee033c534818a083b3763
Felix Geller <fgeller@gmail.com>
2012-03-20 17:24:29 +0100

    No ellipses. Empty instead.

--- init-dired.el
+++ init-dired.el
@@ -3,7 +3,7 @@
 (require 'dired+)
 (setq dired-recursive-deletes 'top)
 (define-key dired-mode-map [mouse-2] 'dired-find-file)
-(setq dired-details-hidden-string "… ")
+(setq dired-details-hidden-string "")
 (require 'dired-details)
 (require 'dired-details+)
 
