34cd6c7a644effede90cee79881508e8727c5bee
Felix Geller <fgeller@gmail.com>
2012-03-27 17:25:15 +0200

    A running compilation should bother flymake.
    I run lots of processes in compilation mode, many of them
    indefinitely (e.g., starting a web server I'm developing on).

--- init-flymake.el
+++ init-flymake.el
@@ -1,6 +1,7 @@
 ;; don't want this on Mac OS X -- http://koansys.com/tech/emacs-hangs-on-flymake-under-os-x
 (setq flymake-gui-warnings-enabled nil)
 (setq flymake-start-syntax-check-on-newline t)
+(setq flymake-compilation-prevents-syntax-check nil)
 
 (require 'flymake)
 (require 'flymake-cursor)
