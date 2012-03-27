7e7942d27ae96b30134edb815b85008daee7ae5d
Felix Geller <fgeller@gmail.com>
2012-03-27 17:24:24 +0200

    Drop configuration I don't use.

--- init-flymake.el
+++ init-flymake.el
@@ -2,17 +2,6 @@
 (setq flymake-gui-warnings-enabled nil)
 (setq flymake-start-syntax-check-on-newline t)
 
-;; Stop flymake from breaking when ruby-mode is invoked by mmm-mode,
-;; at which point buffer-file-name is nil
-(eval-after-load 'flymake
-  '(progn
-         (defun flymake-can-syntax-check-file (file-name)
-           "Determine whether we can syntax check FILE-NAME.
-Return nil if we cannot, non-nil if we can."
-           (if (and file-name (flymake-get-init-function file-name)) t nil))
-         ))
-
-
 (require 'flymake)
 (require 'flymake-cursor)
 
