48aaef95aea718dbf65ac77a0cd2b588a861bf25
Felix Geller <fgeller@gmail.com>
2012-03-28 13:51:08 +0200

    Eshell prompt customization.
     - Show git branch info if applicable
    
     - Re-format tramp name to @remotehost /path/to/wd

--- init-eshell.el
+++ init-eshell.el
@@ -9,4 +9,30 @@
 (setq eshell-review-quick-commands nil)
 (setq eshell-smart-space-goes-to-end t)
 
+(require 'vc-git)
+(defun fg/eshell-git-info ()
+  (let* ((branch (vc-git-working-revision (eshell/pwd))))
+    (if (not (string-equal "" branch))
+        (concat branch " ")
+      "")))
+
+
+(defun fg/eshell-replace-prompt-prefixes ()
+  (let ((absolute-path (eshell/pwd)))
+    (cond ((string-match (getenv "HOME") absolute-path)
+           (replace-match "~" nil nil absolute-path))
+          ((string-match "/ssh:\\(.+\\):" absolute-path)
+           (replace-match (concat "@" (match-string 1 absolute-path) " ")  nil nil absolute-path))
+          (t
+           absolute-path))))
+
+(defun fg/eshell-prompt-function ()
+  (concat
+   (fg/eshell-git-info)
+   (fg/eshell-replace-prompt-prefixes)
+   "/ "))
+
+
+(setq eshell-prompt-function #'fg/eshell-prompt-function)
+(setq eshell-prompt-regexp "^[^\n]*/ ")
 (provide 'init-eshell)
