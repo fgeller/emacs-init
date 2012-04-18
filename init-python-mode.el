bba125fa4df492f394bd55e536a63136d8c33b14
Felix Geller <fgeller@gmail.com>
2012-04-18 16:59:39 +0200

    Drop Pymacs altogether (leftover from ropemacs configuration)

--- init-python-mode.el
+++ init-python-mode.el
@@ -1,16 +1,3 @@
-;;;;;;;;;;;;
-;; Pymacs ;;
-;;;;;;;;;;;;
-(require 'pymacs "~/.emacs.d/site-lisp/Pymacs/pymacs.el")
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ropemacs ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(autoload 'pymacs-apply "pymacs")
-(autoload 'pymacs-call "pymacs")
-(autoload 'pymacs-eval "pymacs" nil t)
-(autoload 'pymacs-exec "pymacs" nil t)
-(autoload 'pymacs-load "pymacs" nil t)
-
 ;;;;;;;;;;;;;;;;;
 ;; python-mode ;;
 ;;;;;;;;;;;;;;;;;
