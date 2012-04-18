2c601b5930559822ab92a96af17e5c6d0c9e297a
Felix Geller <fgeller@gmail.com>
2012-04-18 11:52:32 +0200

    Drop smart eshell config.

--- init-eshell.el
+++ init-eshell.el
@@ -4,11 +4,6 @@
 (eval-after-load 'eshell
   '(add-to-list 'eshell-visual-commands "htop"))
 
-(require 'em-smart)
-(setq eshell-where-to-jump 'begin)
-(setq eshell-review-quick-commands nil)
-(setq eshell-smart-space-goes-to-end t)
-
 (require 'vc-git)
 (defun fg/eshell-git-info ()
   (let* ((branch (vc-git-working-revision (eshell/pwd))))
