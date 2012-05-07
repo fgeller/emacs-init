0ea8730bff58424c87eda25f8f727f7a7dc03fae
Felix Geller <fgeller@gmail.com>
2012-05-07 12:58:01 +0200

    Save tramp files locally.

--- init-tramp.el
+++ init-tramp.el
@@ -2,5 +2,6 @@
 (add-to-list 'Info-default-directory-list "~/.emacs.d/addons/tramps/info/")
 (setq remote-file-name-inhibit-cache nil)
 
+(setq tramp-auto-save-directory "~/.emacs.d/saved/")
 
 (provide 'init-tramp)
