085d0ed3b18cd44d5a9fa3172078a34f5ac9ff7d
Felix Geller <fgeller@gmail.com>
2012-05-30 11:36:24 +0200

    Special grep configuration file.

--- init-editing-utils.el
+++ init-editing-utils.el
@@ -38,16 +38,6 @@
  compilation-message-face nil
  )
 
-(require 'grep)
-(require 'wgrep)
-(add-to-list 'grep-find-ignored-directories "env")
-(setq-default find-program "gfind")
-(grep-apply-setting 'grep-find-command "gfind . -type f -exec grep -nH -e  {} +")
-(grep-apply-setting 'grep-find-template "gfind . <X> -type f <F> -exec grep <C> -nH -e <R> {} +")
-(defun fg/rgrep ()
-  (let ((default-directory (fg/guess-project-root ())))
-    (call-interactively 'rgrep)))
-
 (require 'multiple-cursors)
 
 (add-hook 'find-file-hooks 'goto-address-prog-mode)
